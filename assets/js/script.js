0bd083368808467abb7538267180aea40c0c9d00
sinka27 <sinka27392@gmail.com>
2022-01-02 16:04:23 -0800

    Defined onclick event for the START button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,19 @@
+var startButton = document.getElementById("startbutton");
 var timeEl = document.getElementById("time");
 var secondsLeft = 60;
 
-function setTime(){
-    var timeInterval = setInterval(function() {
-        secondsLeft--;
-        timeEl.textContent = secondsLeft;
-        if(secondsLeft===0){
-            clearInterval(timeInterval);
-        }
-    },1000);
+//creating countdown of 60 seconds
+function setTime() {
+  var timeInterval = setInterval(function () {
+    secondsLeft--;
+    timeEl.textContent = secondsLeft;
+    if (secondsLeft === 0) {
+      clearInterval(timeInterval);
+    }
+  }, 1000);
 }
-setTime();
+
+//making sure countdown starts only when user clicks on start button
+startButton.onclick = function () {
+  setTime();
+};
