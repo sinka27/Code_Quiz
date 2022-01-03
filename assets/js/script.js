ca4721e38144334ee8d280e630aa179e2bcd3178
sinka27 <sinka27392@gmail.com>
2022-01-02 23:09:32 -0800

    1. Added question container to display questions with multiple options as answers  2. Updated related CSS and JS files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,11 @@
+//var startPath = "./assets/HTML/start.html";
 var startButton = document.getElementById("startbutton");
+var welcomeContainer = document.getElementById("welcome-container");
+var questionContainer = document.getElementById("question-container");
 var timeEl = document.getElementById("time");
 var secondsLeft = 60;
 
-//creating countdown of 60 seconds
+//creating timer of 60 seconds
 function setTime() {
   var timeInterval = setInterval(function () {
     secondsLeft--;
@@ -15,5 +18,37 @@ function setTime() {
 
 //making sure countdown starts only when user clicks on start button
 startButton.onclick = function () {
+  //calling our timer function
   setTime();
+
+  welcomeContainer.classList.add("hide");
+
+  questionContainer.classList.remove("hide");
+
+  //calling question function
 };
+
+// function mainPopulate(file, callbackFunction) {
+//     fetch(file)
+//         .then(function (response) {
+//             return response.text();
+//         })
+//         .then(function (text) {
+//             document.getElementById('main').innerHTML = text;
+
+//             if (callbackFunction !== null) {
+//                 callbackFunction();
+//             }
+//         })
+// }
+
+// function updateStartButton() {
+//     var startButton = document.getElementById("startbutton");
+//     startButton.onclick = function () {
+//       //calling our timer function
+//       setTime();
+
+//       //calling our question function
+//     };
+// }
+// mainPopulate(startPath, updateStartButton);
