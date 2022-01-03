61f468aa04c1236e2cdc13f0b5617aa284091f47
sinka27 <sinka27392@gmail.com>
2022-01-03 00:30:14 -0800

    1. Displaying 1 question with multiple choice answers on clicking start button  2. Applied background image

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,11 @@
 var startButton = document.getElementById("startbutton");
 var welcomeContainer = document.getElementById("welcome-container");
 var questionContainer = document.getElementById("question-container");
+var questionEl = document.getElementById("question");
+var answerButtonsEl = document.getElementById("answerbuttons");
 var timeEl = document.getElementById("time");
 var secondsLeft = 60;
+var shuffledQuestion, currentQuestionIndex;
 
 //creating timer of 60 seconds
 function setTime() {
@@ -17,16 +20,47 @@ function setTime() {
 }
 
 //making sure countdown starts only when user clicks on start button
-startButton.onclick = function () {
+startButton.addEventListener("click", startQuiz);
+
+function startQuiz() {
   //calling our timer function
   setTime();
 
+  //hiding welcome note and start button
   welcomeContainer.classList.add("hide");
 
+  //currentQuestionIndex=0;
+
+  //displaying questions and answer options
   questionContainer.classList.remove("hide");
 
   //calling question function
-};
+  showQuestion();
+}
+
+function showQuestion() {
+  questionEl.innerText = questions[0].question;
+  answerButtonsEl.innerText = "";
+  questions[0].answers.forEach((answer) => {
+    const button = document.createElement("button");
+    button.innerText = answer.text;
+    button.classList.add("button");
+    answerButtonsEl.appendChild(button);
+  });
+}
+//creating array of different questions
+var questions = [
+  {
+    question:
+      "______ tag is an extension to HTML that can enclose any number of JavaScript statements.",
+    answers: [
+      { text: "1. <SCRIPT>", correct: true },
+      { text: "2. <BODY>", correct: false },
+      { text: "3. <HEAD>", correct: false },
+      { text: "4. <TITLE>", correct: false },
+    ],
+  },
+];
 
 // function mainPopulate(file, callbackFunction) {
 //     fetch(file)
